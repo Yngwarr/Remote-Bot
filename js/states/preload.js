af03addb4612a47fce7cf258523eb998aa770105
Yngwarr <k.yngvarr@gmail.com>
2018-04-21 18:24:56 +0700

    Tilemaps here!

--- js/states/preload.js
+++ js/states/preload.js
@@ -1,5 +1,9 @@
 states['preload'] = {
-	preload: () => {},
+	preload: () => {
+		game.load.tilemap('world', 'map/level0.csv');
+		game.load.image('tiles', 'img/tilemap.png');
+		game.load.image('player', 'img/dawg.png');
+	},
 	create: () => {
 		document.querySelector('canvas').oncontextmenu
 			= function() { return false; };
