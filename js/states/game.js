ef54ce6e59d4519d554f77ecb6fda2e4e04bbe6c
Yngwarr <k.yngvarr@gmail.com>
2018-04-23 12:17:22 +0700

    Dekomori-Death

--- js/states/game.js
+++ js/states/game.js
@@ -17,17 +17,11 @@ states['game'] = {
 	init: () => {},
 	create: () => {
 		/* sprite groups for objects */
+		obj['spike_u'] = game.add.group();
 		obj['gate'] = game.add.group();
-		obj['laser'] = game.add.group();
-		obj['bridge'] = game.add.group();
-		obj['spring'] = game.add.group();
-		obj['plate'] = game.add.group();
-		obj['door'] = game.add.group();
 		obj['door_r'] = game.add.group();
 		obj['door_g'] = game.add.group();
 		obj['door_b'] = game.add.group();
-		obj['turret'] = game.add.group();
-		obj['lever'] = game.add.group();
 		obj['card_r'] = game.add.group();
 		obj['card_g'] = game.add.group();
 		obj['card_b'] = game.add.group();
@@ -78,6 +72,9 @@ states['game'] = {
 		player.on_ladder[1] = false;
 
 		game.physics.arcade.collide(player, layer);
+		game.physics.arcade.overlap(player, obj['spike_u'], (us, them) => {
+			die();
+		});
 		game.physics.arcade.overlap(player, obj['gate']);
 		game.physics.arcade.overlap(player, obj['anomaly'], (us, them) => {
 			player.on_ladder[1] = true;
@@ -123,21 +120,26 @@ function climb() {
 	holded_cmd = '';
 }
 
+function die() {
+	/* TODO play animation */
+	player.is_climbing = false;
+	player.is_stopped = true;
+	player.body.gravity.y = GRAVITY;
+	player.body.velocity.x = 0;
+	player.body.velocity.y = 0;
+	player.body.position.x = active_gate.body.position.x;
+	player.body.position.y = active_gate.body.position.y;
+}
+
 function populate(map, layer) {
 	/* set collidable tiles */
-	map.setCollisionBetween(1,2);
+	map.setCollisionBetween(1,1);
 	map.setCollisionBetween(5,6);
 	map.setCollisionBetween(10,10);
 	map.setCollisionBetween(16,18);
 	/* turn tiles into sprites */
+	map.createFromTiles(2, 0, 'spike_u', layer, obj['spike_u']);
 	map.createFromTiles(4, 0, 'gate', layer, obj['gate']);
-	map.createFromTiles(5, 0, 'laser', layer, obj['laser']);
-	map.createFromTiles(7, 0, 'bridge', layer, obj['bridge']);
-	map.createFromTiles(8, 0, 'spring', layer, obj['spring']);
-	map.createFromTiles(9, 0, 'plate', layer, obj['plate']);
-	map.createFromTiles(10, 0, 'door', layer, obj['door']);
-	map.createFromTiles(11, 0, 'turret', layer, obj['turret']);
-	map.createFromTiles(12, 0, 'lever', layer, obj['lever']);
 	map.createFromTiles(13, 0, 'card_r', layer, obj['card_r']);
 	map.createFromTiles(14, 0, 'card_g', layer, obj['card_g']);
 	map.createFromTiles(15, 0, 'card_b', layer, obj['card_b']);
@@ -183,53 +185,16 @@ function populate(map, layer) {
 			/* TODO add a particle effect */
 		});
 	}, this);
-	obj['laser'].forEach((sp) => {
-		/* TODO add more animation */
-		sp.animations.add('idle', [0], 30, true);
-		sp.animations.play('idle');
-	}, this);
-	obj['spring'].forEach((sp) => {
-		/* TODO add interaction */
-		sp.animations.add('idle', [0], 30, true);
-		sp.animations.add('press', [1,2,3,4,4,5,5], 15).onComplete
-			.add(function () {
-				this.animations.play('release');
-			}, sp);
-		sp.animations.add('release', [4,3,2,1], 60).onComplete
-			.add(function () {
-				this.animations.play('idle');
-			}, sp);
-		sp.animations.play('idle');
-	}, this);
-	obj['door'].forEach(anim_door, this);
 	obj['door_r'].forEach(anim_door, this);
 	obj['door_g'].forEach(anim_door, this);
 	obj['door_b'].forEach(anim_door, this);
-	obj['turret'].forEach((sp) => {
-		/* TODO needs rotation */
-		sp.animations.add('idle', [0], 30, true);
-		sp.animations.add('warmup', [1,2,3,4,5,6,7], 15).onComplete
-			.add(function () {
-				this.animations.play('fire');
-			}, sp);
-		sp.animations.add('fire', [7,7,8,8,7,7,8,8,7,7,8,8], 15).onComplete
-			.add(function () {
-				this.animations.play('idle');
-			}, sp);
-		sp.animations.play('idle');
-	}, this);
-	obj['lever'].forEach((sp) => {
-		sp.animations.add('idle', [0], 30, true);
-		sp.animations.add('toggle', [1,2]).onComplete.add(function () {
-			this.animations.play('turned');
-		}, sp);
-		sp.animations.add('turned', [3], 30, true);
-		sp.animations.play('idle');
-	}, this);
 	obj['card_r'].forEach(anim_card, this);
 	obj['card_g'].forEach(anim_card, this);
 	obj['card_b'].forEach(anim_card, this);
 	obj['anomaly'].forEach((sp) => {
+		game.physics.arcade.enable(sp);
+	}, this);
+	obj['spike_u'].forEach((sp) => {
 		game.physics.arcade.enable(sp);
 	}, this);
 }
